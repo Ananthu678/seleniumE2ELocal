2fd0266a8a7706a8cbdf73bc5e9466b4e69f177f
I322911 <anantha.padmanabhan.s@sap.com>
2021-03-11 15:31:36 +0530

    Second commit message

--- src/test/java/com/qa/test/loginTest.java
+++ src/test/java/com/qa/test/loginTest.java
@@ -4,13 +4,13 @@ package com.qa.test;
 
 import org.testng.annotations.Test;
 
-
+import com.qa.page.AnnouncementsPage;
 import com.qa.page.LoginPage;
 
 public class loginTest extends BaseClass {
 
 	   
-	  @Test
+	  @Test (priority=1)
 	  public void LoginPage() {
 		  
 		   LoginPage lp = new LoginPage(driver);
@@ -22,5 +22,12 @@ public class loginTest extends BaseClass {
 		   
 		   
 		    
+	  }
+	  
+	  @Test (priority=2)
+	  public void clickProfile() {
+		  AnnouncementsPage ap=new AnnouncementsPage(driver);
+		  ap.click();
+		  
 	  }
 }
